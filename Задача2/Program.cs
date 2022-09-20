c5a2dc1554419e24939ef83d86488f7f75b6bf73
Nastasya2141 <113186212+Nastasya2141@users.noreply.github.com>
2022-09-20 23:31:43 +0300

    Task2

---
+++ Задача2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Программа, которая принимет два числа и выдает, максимальное и минимальное
+Console.WriteLine("Введите число 1");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+int b = Convert.ToInt32(Console.ReadLine());
+int min = Math.Min(a, b);
+int max = Math.Max(a, b);
+
+Console.WriteLine("Минимум = " +min);
+Console.WriteLine("Максимум = " +max);
