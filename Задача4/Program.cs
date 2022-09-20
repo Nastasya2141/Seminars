6d20b71dbc0f5bcb88ea1944b5a5e1d45c0c96ab
Nastasya2141 <113186212+Nastasya2141@users.noreply.github.com>
2022-09-20 23:32:36 +0300

    Task4

---
+++ Задача4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа для нахождения максимального из трех введенных чисел
+Console.WriteLine("Введите число 1");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+if (b > a)
+    max = b;
+if (c > max)
+    max = c;
+
+Console.WriteLine("Максимум = " +max);
