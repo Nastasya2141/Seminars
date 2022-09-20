cfb2e5621c1f36fbbcf75c14d6e91faf1a8f2ac9
Nastasya2141 <113186212+Nastasya2141@users.noreply.github.com>
2022-09-20 23:33:38 +0300

    Task4

---
+++ Задача6/Program.cs
@@ -0,0 +1,12 @@
+﻿// Является ли введеное пользователем число четным
+Console.WriteLine("Введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a%2==0) 
+{
+Console.WriteLine("Число является чётным");
+}
+else 
+{
+    Console.WriteLine("Число не является чётным");
+}
