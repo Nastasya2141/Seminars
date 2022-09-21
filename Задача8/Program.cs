375cc130dd289e2256e1c2109be881afe927384a
Nastasya2141 <113186212+Nastasya2141@users.noreply.github.com>
2022-09-21 17:31:57 +0300

    Task8

---
+++ Задача8/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа, которая принимает число N,а на выходе выдает только четные число от 1 до N
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 1 ;
+
+while (i<=N)
+{
+    if (i%2==0)
+    {
+        Console.WriteLine(i);
+    }
+i++;
+}
+
+
+
